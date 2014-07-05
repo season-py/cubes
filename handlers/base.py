3e95cd970d2cb4fc799acb72bb8d292765dc31e7
season-py <haishan09@gmail.com>
2014-07-05 13:52:41 +0800

    basehandle adding

---
+++ handlers/base.py
@@ -0,0 +1,26 @@
+from tornado import web
+
+
+class AccessingDeny(Exception):
+    pass
+
+
+class Context(dict):
+
+    def __getattr__(self, key):
+        if not key.startswith('__'):
+            return self[key]
+        else:
+            super(Context, self).__getattr__(key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+
+class BaseHandler(web.RequestHandler):
+
+    def __init__(self, application, request, **kwargs):
+        super(BaseHandler, self).__init__(application, request, **kwargs)
+
+    def prepare(self):
+        self.context = Context()
