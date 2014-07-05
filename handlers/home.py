3a38764e7f34b7d35419e681bc708e2a28f4af6d
season-py <haishan09@gmail.com>
2014-07-05 13:43:03 +0800

    some template file added

--- handlers/home.py
+++ handlers/home.py
@@ -1,14 +1,14 @@
-from tornado import web
-from tornado import gen
-from tornado import log
+from base import BaseHandler
 from utils.urlmap import urlmap
-from models import activity
-from pony.orm import db_session
+from models.activity import Activity
+from pony.orm import db_session, select
+
 
 @urlmap(appkey=[1, 2], url=r'/home')
-class Home(web.RequestHandler):
+class Home(BaseHandler):
 
     def get(self):
-        self.write('Paty Go!!!')
         with db_session:
-            activity.Activity()
+            select(a for a in Activity).order_by(Activity.add_dt)[:]
+        self.context.news = "hello world"
+        self.render('home.html', **self.context)
--- settings.py
+++ settings.py
@@ -1,6 +1,9 @@
 import os
 WORKSPACE = os.path.dirname(os.path.abspath(__file__))
 SETTINGS = {
-        'login_url': 'http://localhost:8888',
-        'template_path': os.path.join(WORKSPACE, 'templates'),
-        'sqlite_path': os.path.join(WORKSPACE, 'sqlite')}
+	'autoreload': True,
+    'login_url': 'http://localhost:8888',
+    'debug': True,
+    'template_path': os.path.join(WORKSPACE, 'templates'),
+    'static_path': os.path.join(WORKSPACE, 'static'),
+    'sqlite_path': os.path.join(WORKSPACE, 'sqlite')}
