9d4e26f5765848e66dc57449d188a1c35073ca35
root <root@private40002.cm-agent.bi.oppo.mobi>
2015-08-31 17:44:04 +0800

    model obj added

--- cube.py
+++ cube.py
@@ -1,12 +1,28 @@
 
-class Cube(object):
+class ModelAttrError(Exception):
+    pass
+
+class ModelObj(object):
 
     @classmethod
-    def from_metadata(cls, metadata, store):
-        assert name in metadata
-        dimensions_links = metadata.pop('dimensions', [])
-        aggregates = metadata.pop('aggregates', [])
-        measures = metadata.pop('measures', [])
+    def from_metadata(cls, metadata):
+        return cls(**metadata)
 
-    def __init__(self):
-        pass
+    def __init__(self, *args, **kwargs):
+        for key, val in kwargs.items():
+            if key in self.attributes:
+                setattr(self, key, val)
+            else:
+                raise ModelAttrError('object {0} has no attribute "{1}"'.format(self.__class__.__name__, key))
+
+class Dimension(ModelObj):
+    attributes = {'name', 'label', 'levels', 'hierarchies', 'master', 'description'}
+
+
+class Measure(ModelObj):
+    attributes = {'name', 'label', 'levels', 'hierarchies', 'master', 'description'}
+
+
+class Cube(object):
+    attributes = {'name', 'label', 'levels', 'hierarchies', 'master', 'description'}
+
