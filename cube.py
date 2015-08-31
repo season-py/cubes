fdee5404308d2c794c89cc552f0dc5b7ed772ac0
root <root@private40002.cm-agent.bi.oppo.mobi>
2015-08-31 19:17:04 +0800

    model parser

--- cube.py
+++ cube.py
@@ -1,6 +1,8 @@
+from collections import defaultdict
 
 class ModelAttrError(Exception):
     pass
+
 
 class ModelObj(object):
 
@@ -10,19 +12,47 @@ class ModelObj(object):
 
     def __init__(self, *args, **kwargs):
         for key, val in kwargs.items():
-            if key in self.attributes:
+            if key in self._attributes:
                 setattr(self, key, val)
             else:
                 raise ModelAttrError('object {0} has no attribute "{1}"'.format(self.__class__.__name__, key))
+        self._init_lazy()
+
+    def _init_lazy(self):
+        self.label = self.label or self.name
+        if self.dimensions:
+            self.dimensions = [self._create_dimension(dim) for dim in self.dimensions]
+ 
+    def _create_dimension(self, dim):
+        if isinstance(dim, str):
+            return self._instances[dim]
+        elif isinstance(dim, dict):
+            return Dimension.from_metadata(dim)
+
+    def __getattr__(self, key):
+        if key.startswith('__') and key.endswith('__'):
+            val = super(ModelObj).__getattr__(key)
+        try:
+            val = getattr(self.__dict__, key)
+        except AttributeError, e:
+            val = None
+        return val
+
 
 class Dimension(ModelObj):
-    attributes = {'name', 'label', 'levels', 'hierarchies', 'master', 'description'}
+
+    _instances = {}
+    _attributes = {'name', 'label', 'levels', 'hierarchies', 'master', 'description'}
 
 
 class Measure(ModelObj):
-    attributes = {'name', 'label', 'levels', 'hierarchies', 'master', 'description'}
+
+    _instances = {}
+    _attributes = {'name', 'label', 'function'}
 
 
 class Cube(object):
-    attributes = {'name', 'label', 'levels', 'hierarchies', 'master', 'description'}
+
+    _instances = {}
+    _attributes = {'name', 'label', 'store', 'fact', 'dimensions', 'measures', 'aggregates', 'mappings', 'joins'}
 
